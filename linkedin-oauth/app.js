750afb6055ec3caef82d4dece385e76fd20bceac
Nathan Reginato <nathan.reginato@gmail.com>
2016-04-05 20:34:14 -0600

    Finished

--- linkedin-oauth/app.js
+++ linkedin-oauth/app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
+var TwitterStrategy = require('passport-twitter').Strategy;
 var passport = require('passport')
 var cookieSession = require('cookie-session')
 
@@ -24,11 +25,11 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: false
+    extended: false
 }));
 app.use(cookieSession({
-  name: 'session',
-  keys: ['key1', 'key2']
+    name: 'session',
+    keys: ['key1', 'key2']
 }))
 app.use(cookieParser());
 app.use(passport.initialize());
@@ -36,40 +37,78 @@ app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 passport.use(new LinkedInStrategy({
-  clientID: process.env.LINKEDIN_CLIENT_ID,
-  clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
-  callbackURL: process.env.HOST + "/auth/linkedin/callback",
-  scope: ['r_emailaddress', 'r_basicprofile'],
-  state: true
+    clientID: process.env.LINKEDIN_CLIENT_ID,
+    clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
+    callbackURL: process.env.HOST + "/auth/linkedin/callback",
+    scope: ['r_emailaddress', 'r_basicprofile'],
+    state: true
 }, function(accessToken, refreshToken, profile, done) {
-  done(null, {id: profile.id, displayName: profile.displayName })
-
+    done(null, {
+        id: profile.id,
+        displayName: profile.displayName,
+        token: accessToken
+    })
 }));
+
+passport.use(new TwitterStrategy({
+        consumerKey: process.env.TWITTER_CONSUMER_KEY,
+        consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+        callbackURL: process.env.HOST + "/auth/twitter/callback"
+    }, function(token, tokenSecret, profile, cb) {
+        console.log(profile.username);
+        return cb(null, profile);
+  }));
 
 app.get('/auth/linkedin/callback', passport.authenticate('linkedin', {
-  successRedirect: '/',
-  failureRedirect: '/login'
+    successRedirect: '/',
+    failureRedirect: '/login'
 }));
 
-app.get('/auth/linkedin',
-  passport.authenticate('linkedin'),
+app.get('/auth/twitter/callback',
+  passport.authenticate('twitter', { failureRedirect: '/login' }),
   function(req, res) {
-    // The request will be redirected to LinkedIn for authentication, so this
-    // function will not be called.
+    // Successful authentication, redirect home.
+    res.redirect('/');
   });
 
+
+app.get('/auth/linkedin',
+    passport.authenticate('linkedin'),
+    function(req, res) {
+        // The request will be redirected to LinkedIn for authentication, so this
+        // function will not be called.
+    });
+app.get('/auth/twitter',
+    passport.authenticate('twitter'),
+    function(req, res) {
+
+        // The request will be redirected to LinkedIn for authentication, so this
+        // function will not be called.
+    });
+
+
+// passport.serializeUser(function(user, done) {
+//     done(null, user);
+// });
+//
+// passport.deserializeUser(function(user, done) {
+//     done(null, user);
+// });
+
 passport.serializeUser(function(user, done) {
-  done(null, user);
+  console.log('yay');
+    done(null, user);
 });
 
 passport.deserializeUser(function(user, done) {
-  done(null, user);
+  console.log('yay');
+    done(null, user);
 });
 
-app.use(function (req, res, next) {
-  console.log(req.user);
-  res.locals.user = req.user
-  next()
+app.use(function(req, res, next) {
+    console.log(req.user);
+    res.locals.user = req.user
+    next()
 })
 
 app.use('/', routes);
@@ -77,9 +116,9 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers`
@@ -87,23 +126,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
--- linkedin-oauth/routes/index.js
+++ linkedin-oauth/routes/index.js
@@ -5,12 +5,17 @@ var unirest = require('unirest');
 /* GET home page. */
 
 router.get('/', function(req, res, next) {
+  res.render('index');
+})
+
+router.get('/', function(req, res, next) {
   if(req.isAuthenticated()) {
     unirest.get('https://api.linkedin.com/v1/people/~:(id,num-connections,picture-url)')
       .header('Authorization', 'Bearer ' + req.user.token)
       .header('x-li-format', 'json')
       .end(function (response) {
         res.render('index', { profile: response.body });
+        console.log(response.body);
       })
   } else {
     res.render('index', {  });
